df246d2cff4d219d9d4a853904d4e324b33ef74f
makmzw <makmzw@gmail.com>
2017-07-01 08:14:54 +0000

    init

---
+++ test.rb
@@ -0,0 +1,19 @@
+class MyError < StandardError; end
+
+x = gets.to_i
+
+begin
+if x == 3
+  raise MyError
+end
+p 100 / x
+rescue => ex
+puts "not 3!"
+rescue => ex
+
+p ex.message
+p ex.class
+puts "stopped!"
+ensure
+puts "--END--"
+end
